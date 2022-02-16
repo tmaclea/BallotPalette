4b47ec212d82631f28f6800886fac75d6163a577
tmaclea <macleanis@hotmail.com>
2022-02-16 15:19:38 -0700

    Basic result page added
    Added basic, blank result page that works before doing anything crazy like adding a library or something

---
+++ BallotPalette/Pages/Ballots/Results.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BallotPalette.Core;
+using BallotPalette.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BallotPalette.Pages.Ballots
+{
+    public class ResultsModel : PageModel
+    {
+        private readonly IBallotData ballotData;
+
+        public Ballot Ballot { get; set; }
+
+        public ResultsModel(IBallotData ballotData)
+        {
+            this.ballotData = ballotData;
+        }
+
+        public IActionResult OnGet(int ballotId)
+        {
+            Ballot = ballotData.GetBallotById(ballotId);
+
+            if (Ballot == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
